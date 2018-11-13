a4d756db055f22e578f7dce3ef7bf0dfc39d4d10
Ebrahim <naeimi.e@gmail.com>
2018-11-13 14:38:19 +0100

    Test push changes

--- VSCodeProjects/HelloWorld/Program.cs
+++ VSCodeProjects/HelloWorld/Program.cs
@@ -8,18 +8,9 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-           
-           
-           Console.WriteLine("AAAAAAAAAAAAAAAAAA");
-            //var name = System.Environment.MachineName;
-            //Console.WriteLine("Hello World!"+ name);
-        // BalancedP bp= new BalancedP();
-        
-            // if (bp.Matchbr("{[(])]}")) 
 
-        // Console.WriteLine("Yes");
-        // else 
-        // Console.WriteLine("No");
+            string s;
+            Console.WriteLine(s);
 
         }
     }
